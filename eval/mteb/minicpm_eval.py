c0e23829aa24834f3086e7bcdb5d5d795bab1136
trapoom555 <trapoom555@gmail.com>
2024-04-16 15:00:06 +0800

    add eval script for mteb

---
+++ eval/mteb/minicpm_eval.py
@@ -0,0 +1,44 @@
+from model.minicpm import MiniCPM
+from mteb import MTEB
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("main")
+
+model = MiniCPM()
+
+TASK_LIST_RETRIEVAL = [
+    "ArguAna",
+    "ClimateFEVER",
+    "CQADupstackAndroidRetrieval",
+    "CQADupstackEnglishRetrieval",
+    "CQADupstackGamingRetrieval",
+    "CQADupstackGisRetrieval",
+    "CQADupstackMathematicaRetrieval",
+    "CQADupstackPhysicsRetrieval",
+    "CQADupstackProgrammersRetrieval",
+    "CQADupstackStatsRetrieval",
+    "CQADupstackTexRetrieval",
+    "CQADupstackUnixRetrieval",
+    "CQADupstackWebmastersRetrieval",
+    "CQADupstackWordpressRetrieval",
+    "DBPedia",
+    "FEVER",
+    "FiQA2018",
+    "HotpotQA",
+    "MSMARCO",
+    "NFCorpus",
+    "NQ",
+    "QuoraRetrieval",
+    "SCIDOCS",
+    "SciFact",
+    "Touche2020",
+    "TRECCOVID",
+]
+
+for task in TASK_LIST_RETRIEVAL:
+    logger.info(f"Running task: {task}")
+    evaluation = MTEB(tasks=[task], task_langs=["en"])
+    evaluation.run(model, output_folder=f"results/minicpm")
+
+
