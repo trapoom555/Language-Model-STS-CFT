3bc2f10233d4d9217c1402f2948dbfbfab692b0e
trapoom555 <trapoom555@gmail.com>
2024-04-17 14:50:51 +0800

    can run properly

--- train/train.py
+++ train/train.py
@@ -7,13 +7,15 @@ from torch.utils.data import DataLoader
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 
-BATCH_SIZE = 8
+DESIRE_BATCH_SIZE = 1024
+N_GPUS = 3
 LR = 1e-3
 LORA_RANK = 8
-N_GRAD_ACC = 128
-MAX_EPOCH = 5
+MAX_EPOCH = 2
 
-wandb_logger = WandbLogger(log_model="all")
+BATCH_SIZE = 4  # Fixed for RTX3090 (RAM Limit)
+N_GRAD_ACC = int(DESIRE_BATCH_SIZE / N_GPUS / BATCH_SIZE)
+
 
 ################################# Logger #################################
 
@@ -25,11 +27,7 @@ config = {
     "n_grad_acc": N_GRAD_ACC
 }
 
-wandb.init(
-    project="minicpm-dense-retrieval",
-    config=config
-)
-
+wandb_logger = WandbLogger(project="minicpm-dense-retrieval")
 ##########################################################################
 
 lora_config = LoraConfig(
@@ -39,8 +37,7 @@ lora_config = LoraConfig(
         r=LORA_RANK,
         lora_alpha=32,
         lora_dropout=0.1,
-        inference_mode=False,
-    )
+        inference_mode=False)
 
 checkpoint_callback = ModelCheckpoint(
         save_top_k=1,
@@ -48,25 +45,25 @@ checkpoint_callback = ModelCheckpoint(
         mode="min",
         dirpath="./checkpoint",
         filename="minicpm-{step}-{train_loss:.4f}",
-        every_n_train_steps=10,
-    )
+        every_n_train_steps=10)
 
 dataset = NLIDataset('../data/nli_for_simcse.csv')
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
 model = MiniCPMEncoder(lora_config=lora_config, dataloader=dataloader, lr=LR, n_grad_acc=N_GRAD_ACC)
-wandb_logger.watch(model, log="all")
 
 trainer = L.Trainer(
         max_epochs=MAX_EPOCH, 
-        logger=wandb_logger, 
+        logger=wandb_logger,
+        log_every_n_steps=1,
         accelerator="cuda", 
-        devices=[1], 
+        devices=[1, 2, 3], 
         accumulate_grad_batches=N_GRAD_ACC, 
         callbacks=[checkpoint_callback],
-        precision="bf16"
-    )
+        precision="bf16-mixed",
+        strategy="ddp")
+
+if trainer.global_rank == 0:
+    wandb_logger.experiment.config.update(config)
 
 trainer.fit(model=model)
-
-wandb.finish()
