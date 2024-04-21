7da67cd62c60f21de4e7da957396e21b6e41c340
trapoom555 <trapoom555@gmail.com>
2024-04-21 22:56:35 +0800

    can run but the performance still equal to ddp

--- train/train.py
+++ train/train.py
@@ -1,99 +1,44 @@
-import argparse
-import lightning as L
-from peft import LoraConfig
-from model import MiniCPMEncoder
-from dataset_utils import NLIDataset
-from torch.utils.data import DataLoader
-from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import ModelCheckpoint
+import torch
+from datasets import load_from_disk
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, HfArgumentParser, Trainer, TrainingArguments, set_seed
 
-################################# Parser #################################
+from contrastive_trainer import ContrastiveTrainer
 
-parser = argparse.ArgumentParser(description='MiniCPM Training Script')
-parser.add_argument('--desire_batch_size', required=True)
-parser.add_argument('--init_lr', required=True)
-parser.add_argument('--final_lr', required=True)
-parser.add_argument('--lora_rank', required=True)
-parser.add_argument('--max_epoch', required=True)
-parser.add_argument('--temperature', required=True)
-parser.add_argument('--batch_size_per_gpu', required=True)
-parser.add_argument('--logging', required=True)
-args = vars(parser.parse_args())
+################################# Model + PEFT #################################
 
-DESIRE_BATCH_SIZE = int(args['desire_batch_size'])
-N_GPUS = 3
-LR = float(args['init_lr'])
-FINAL_LR = float(args['final_lr'])
-LORA_RANK = int(args['lora_rank'])
-MAX_EPOCH = int(args['max_epoch'])
-TEMPERATURE = float(args['temperature'])
+model = AutoModelForCausalLM.from_pretrained('../pretrained/MiniCPM-2B-dpo-bf16/', 
+                                            torch_dtype=torch.bfloat16,
+                                            trust_remote_code=True,
+                                            local_files_only=True)
 
-BATCH_SIZE = int(args['batch_size_per_gpu'])  # 8 is safe for RTX3090 (RAM Limit)
-N_GRAD_ACC = int(DESIRE_BATCH_SIZE / N_GPUS / BATCH_SIZE)
-IS_LOG = (True if args['logging'] == 'true' else False)
+lora_config = LoraConfig(init_lora_weights="gaussian",
+                        task_type=TaskType.CAUSAL_LM,
+                        target_modules=["q_proj", "v_proj"],
+                        r=8,
+                        lora_alpha=32,
+                        lora_dropout=0.1,
+                        inference_mode=False)
 
-################################# Logger #################################
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
 
-config = {
-    "desire_batch_size" : DESIRE_BATCH_SIZE,
-    "init_lr": LR,
-    "final_lr": FINAL_LR,
-    "batch_size_per_gpu" : BATCH_SIZE,
-    "lora_rank" : LORA_RANK,
-    "epoch": MAX_EPOCH,
-    "n_grad_acc": N_GRAD_ACC,
-    "n_gpus" : N_GPUS,
-    "temperature" : TEMPERATURE
-}
+################################# Data #################################
 
-if IS_LOG:
-    logger = WandbLogger(project="minicpm-dense-retrieval")
-else:
-    logger = None
-      
-##########################################################################
+train_dataset = load_from_disk('../data/processed')
 
-lora_config = LoraConfig(
-        init_lora_weights="gaussian",
-        task_type="CAUSAL_LM",
-        target_modules=["q_proj", "v_proj"],
-        r=LORA_RANK,
-        lora_alpha=32,
-        lora_dropout=0.1,
-        inference_mode=False)
 
-checkpoint_callback = ModelCheckpoint(
-        save_top_k=1,
-        monitor="train_loss",
-        mode="min",
-        dirpath="./checkpoint",
-        filename="minicpm-{step}-{train_loss:.4f}",
-        every_n_train_steps=10)
 
-dataset = NLIDataset("../data/processed")
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+parser = HfArgumentParser((TrainingArguments))
 
-model = MiniCPMEncoder(lora_config=lora_config,
-                       dataloader=dataloader,
-                       lr=LR,
-                       n_grad_acc=N_GRAD_ACC,
-                       max_epochs=MAX_EPOCH,
-                       final_lr = FINAL_LR,
-                       n_gpus=N_GPUS,
-                       temperature=TEMPERATURE)
+training_args = parser.parse_args_into_dataclasses()[0]
 
-trainer = L.Trainer(
-        max_epochs=MAX_EPOCH, 
-        logger=logger,
-        log_every_n_steps=1,
-        accelerator="cuda", 
-        devices=[0, 1, 2], 
-        accumulate_grad_batches=N_GRAD_ACC, 
-        callbacks=[checkpoint_callback],
-        precision="bf16-mixed",
-        strategy="ddp")
+set_seed(training_args.seed)
 
-if IS_LOG and (trainer.global_rank == 0):
-    logger.experiment.config.update(config)
+trainer = ContrastiveTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset
+)
 
-trainer.fit(model=model)
+trainer.train()
--- train/train.sh
+++ train/train.sh
@@ -1,9 +1,8 @@
-python train.py \
---batch_size_per_gpu 8 \
---desire_batch_size 512 \
---init_lr 1e-4 \
---final_lr 1e-5 \
---temperature 0.05 \
---lora_rank 8 \
---max_epoch 2 \
---logging true
+accelerate launch --config_file ./configs/default_config.yaml train.py \
+--output_dir output/$formatted_time/ \
+--learning_rate 5e-5 --per_device_train_batch_size 8 \
+--bf16 --gradient_accumulation_steps 1 --warmup_steps 100 \
+--max_steps 3000 --weight_decay 0.01 \
+--evaluation_strategy steps --eval_steps 500 \
+--save_strategy steps --save_steps 500 --seed 42 \
+--log_level info --logging_strategy steps --logging_steps 10 --remove_unused_columns false
