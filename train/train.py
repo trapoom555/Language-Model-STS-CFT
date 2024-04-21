6ff8f2e8cef48e71b7a0b5c460c1dd1dc1fa496f
trapoom555 <trapoom555@gmail.com>
2024-04-22 01:57:14 +0800

    log to wandb

--- train/train.py
+++ train/train.py
@@ -1,14 +1,15 @@
+import os
 import torch
 from datasets import load_from_disk
-from peft import LoraConfig, TaskType, get_peft_model
-from transformers import AutoModelForCausalLM, HfArgumentParser, Trainer, TrainingArguments, set_seed
-
 from contrastive_trainer import ContrastiveTrainer
+from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, HfArgumentParser, TrainingArguments, set_seed
 
 def main(training_args):
-    ################################# Model + PEFT #################################
+    set_seed(training_args.seed)
 
-    model = AutoModelForCausalLM.from_pretrained('../pretrained/MiniCPM-2B-dpo-bf16/', 
+    # Model + PEFT
+    model = AutoModelForCausalLM.from_pretrained("../pretrained/MiniCPM-2B-dpo-bf16/", 
                                                 torch_dtype=torch.bfloat16,
                                                 trust_remote_code=True,
                                                 local_files_only=True)
@@ -23,21 +24,16 @@ def main(training_args):
 
     model = get_peft_model(model, lora_config)
 
-    ################################# Data #################################
+    # Data
+    train_dataset = load_from_disk("../data/processed")
 
-    train_dataset = load_from_disk('../data/processed')
-
-    set_seed(training_args.seed)
-
-    trainer = ContrastiveTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset
-    )
+    trainer = ContrastiveTrainer(model=model,
+                                args=training_args,
+                                train_dataset=train_dataset)
 
     trainer.accelerator.print(f"{trainer.model}")
 
-    # handle PEFT+FSDP case
+    # Handle PEFT+FSDP case
     trainer.model.print_trainable_parameters()
     if getattr(trainer.accelerator.state, "fsdp_plugin", None):
         from peft.utils.other import fsdp_auto_wrap_policy
@@ -45,22 +41,21 @@ def main(training_args):
         fsdp_plugin = trainer.accelerator.state.fsdp_plugin
         fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(trainer.model)
 
-    
     print("FSDP Enable", trainer.is_fsdp_enabled)
 
-
-    # train
+    # Train
     checkpoint = None
     if training_args.resume_from_checkpoint is not None:
         checkpoint = training_args.resume_from_checkpoint
     trainer.train(resume_from_checkpoint=checkpoint)
 
-    # saving final model
+    # Saving final model
     if trainer.is_fsdp_enabled:
         trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
     trainer.save_model(training_args.output_dir)
 
 if __name__ == "__main__":
+    os.environ["WANDB_PROJECT"] = "minicpm-dense-retrieval"
     parser = HfArgumentParser((TrainingArguments))
     training_args = parser.parse_args_into_dataclasses()[0]
     main(training_args)
