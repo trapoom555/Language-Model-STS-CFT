47db4a6565caebec86f52ce3ed6bc5f353469ae7
trapoom555 <trapoom555@gmail.com>
2024-04-21 23:51:43 +0800

    settings in hf

--- train/train.py
+++ train/train.py
@@ -5,40 +5,61 @@ from transformers import AutoModelForCausalLM, HfArgumentParser, Trainer, Traini
 
 from contrastive_trainer import ContrastiveTrainer
 
-################################# Model + PEFT #################################
+def main(training_args):
+    ################################# Model + PEFT #################################
 
-model = AutoModelForCausalLM.from_pretrained('../pretrained/MiniCPM-2B-dpo-bf16/', 
-                                            torch_dtype=torch.bfloat16,
-                                            trust_remote_code=True,
-                                            local_files_only=True)
+    model = AutoModelForCausalLM.from_pretrained('../pretrained/MiniCPM-2B-dpo-bf16/', 
+                                                torch_dtype=torch.bfloat16,
+                                                trust_remote_code=True,
+                                                local_files_only=True)
 
-lora_config = LoraConfig(init_lora_weights="gaussian",
-                        task_type=TaskType.CAUSAL_LM,
-                        target_modules=["q_proj", "v_proj"],
-                        r=8,
-                        lora_alpha=32,
-                        lora_dropout=0.1,
-                        inference_mode=False)
+    lora_config = LoraConfig(init_lora_weights="gaussian",
+                            task_type=TaskType.CAUSAL_LM,
+                            target_modules=["q_proj", "v_proj"],
+                            r=8,
+                            lora_alpha=32,
+                            lora_dropout=0.1,
+                            inference_mode=False)
 
-model = get_peft_model(model, lora_config)
-model.print_trainable_parameters()
+    model = get_peft_model(model, lora_config)
 
-################################# Data #################################
+    ################################# Data #################################
 
-train_dataset = load_from_disk('../data/processed')
+    train_dataset = load_from_disk('../data/processed')
+
+    set_seed(training_args.seed)
+
+    trainer = ContrastiveTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset
+    )
+
+    trainer.accelerator.print(f"{trainer.model}")
+
+    # handle PEFT+FSDP case
+    trainer.model.print_trainable_parameters()
+    if getattr(trainer.accelerator.state, "fsdp_plugin", None):
+        from peft.utils.other import fsdp_auto_wrap_policy
+
+        fsdp_plugin = trainer.accelerator.state.fsdp_plugin
+        fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(trainer.model)
+
+    
+    print("FSDP Enable", trainer.is_fsdp_enabled)
 
 
+    # train
+    checkpoint = None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    trainer.train(resume_from_checkpoint=checkpoint)
 
-parser = HfArgumentParser((TrainingArguments))
+    # saving final model
+    if trainer.is_fsdp_enabled:
+        trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
 
-training_args = parser.parse_args_into_dataclasses()[0]
-
-set_seed(training_args.seed)
-
-trainer = ContrastiveTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset
-)
-
-trainer.train()
+if __name__ == "__main__":
+    parser = HfArgumentParser((TrainingArguments))
+    training_args = parser.parse_args_into_dataclasses()[0]
+    main(training_args)
