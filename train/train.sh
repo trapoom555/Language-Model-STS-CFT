daba860ea27bcefd24dfa6cd9dd0f8376ed29446
trapoom555 <trapoom555@gmail.com>
2024-04-22 02:28:43 +0800

    update training config

--- train/train.sh
+++ train/train.sh
@@ -4,9 +4,9 @@ echo $formatted_time
 accelerate launch --config_file ./configs/fsdp_config.yaml train.py \
 --output_dir output/$formatted_time/ \
 --learning_rate 5e-5 --per_device_train_batch_size 10 \
---bf16 --gradient_accumulation_steps 12 --warmup_steps 100 \
+--bf16 --gradient_accumulation_steps 3 --warmup_steps 100 \
 --max_steps 1000 --weight_decay 0.01 \
---save_strategy steps --save_steps 500 --seed 42 \
---log_level info --logging_strategy steps --logging_steps 1 \
+--save_strategy steps --save_steps 500 --seed 7 \
+--log_level info --logging_strategy steps --logging_steps 10 \
 --remove_unused_columns false \
 --report_to wandb
