c205209f03fe4e018c653c6d8586db5ed727c165
trapoom555 <trapoom555@gmail.com>
2024-04-22 00:50:23 +0800

    can train with batch size 10

--- train/train.sh
+++ train/train.sh
@@ -1,6 +1,6 @@
-accelerate launch --config_file ./configs/default_config.yaml train.py \
+accelerate launch --config_file ./configs/fsdp_config.yaml train.py \
 --output_dir output/$formatted_time/ \
---learning_rate 5e-5 --per_device_train_batch_size 8 \
+--learning_rate 5e-5 --per_device_train_batch_size 10 \
 --bf16 --gradient_accumulation_steps 1 --warmup_steps 100 \
 --max_steps 3000 --weight_decay 0.01 \
 --evaluation_strategy steps --eval_steps 500 \
